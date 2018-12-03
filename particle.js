fb1b9635080e6bd43a67ade7897f309fa36133f1
Rookitmin <42980035+Rookitmin@users.noreply.github.com>
2018-12-03 14:11:40 -0500

    Create particle.js

---
+++ particle.js
@@ -0,0 +1,41 @@
+function person() {
+  this.pos = createVector(5, 0);
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+
+  this.applyForce = function(force) {
+    // var f = force;
+    this.acc.add(force);
+  }
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  this.display = function() {
+    fill(255, 150);
+    stroke(255);
+		fill(195);
+		noStroke();
+    rect(this.pos.x, this.pos.y, 40, 50);
+  }
+
+  this.edges = function() {
+    if (this.pos.y + 50 > height) {
+      this.vel.y *= -1;
+      this.pos.y = height - 50;
+    }
+		
+		if (this.pos.y < 0) {
+			this.vel.y *= -1;
+      this.pos.y = 0;
+		}
+
+    if (this.pos.x > width) {
+      this.vel.x *= -1;
+      this.pos.x = width;
+    }
+  }
+}
